1479e15dbe95e19a449f2d3f448ff9116c03e8ac
Christian <cedenoc181@gmail.com>
2024-02-15 21:59:23 -0500

    added schema on to wireframe

--- theratable-files/db/migrate/20240213035948_create_accounts.rb
+++ theratable-files/db/migrate/20240213035948_create_accounts.rb
@@ -1,19 +1,16 @@
 class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
-      t.integer :number
-      t.string :type
-      t.string :username
+      t.integer :account_number #account index number for data query
+      t.string :account_type #client or flex 
+      t.string :email
       t.string :password_digest
-      t.boolean :admin
-      t.string :activation_token
-      t.string :reset_password_token
-      t.string :confirmation_token
-      t.string :billing_address
-      t.string :payment_method
+      t.boolean :admin, default: false
+      t.string :reset_password_token #Token used for password reset functionality
+      t.string :confirmation_token #Token used for email confirmation
       t.string :subscription_plan
       t.date :subscription_expiry_date
-
+      t.integer :payment_method_id
       t.timestamps
     end
   end
