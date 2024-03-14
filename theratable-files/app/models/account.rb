9ab27d6e3aca13f9bf97a81ff32b80911698360b
Christian <cedenoc181@gmail.com>
2024-03-14 13:32:19 -0400

    looking to finalize modles currently looking at the account.rb rrelationship table, also inputting the main idea/ function of tables into excalidraw on the left of diagram

--- theratable-files/app/models/account.rb
+++ theratable-files/app/models/account.rb
@@ -1,2 +1,22 @@
 class Account < ApplicationRecord
+    has_one :user, dependent: :destroy, :inverse_of => :account
+    accepts_nested_attributes_for :user
+    
+    # flexologists
+    has_one :flexologist, dependent: :destroy, :inverse_of => :account
+    accepts_nested_attributes_for :flexologist
+    
+    # Clients
+    has_one :client, dependent: :destroy, :inverse_of => :account
+    accepts_nested_attributes_for :client
+
+    has_many :schedules, through: :flexologist, dependent: :destroy
+    accepts_nested_attributes_for :schedules
+    
+    # bookings
+    has_many :bookings, through: :clients,  dependent: :destroy
+    accepts_nested_attributes_for :bookings
+    
+    has_many :payments, through: :bookings, dependent: :destroy
+    accepts_nested_attributes_for :payments
 end
--- theratable-files/app/models/payment.rb
+++ theratable-files/app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
 
-    belongs_to :account, :inverse_of => :payments
-    accepts_nested_attributes_for :account
+    belongs_to :client
+    accepts_nested_attributes_for :client
      
     belongs_to :booking, :inverse_of => :payments
     accepts_nested_attributes_for :booking
