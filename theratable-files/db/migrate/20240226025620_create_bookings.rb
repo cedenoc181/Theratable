c6145ff629b031310e40aa1602203d78dfb6bf0d
Christian <cedenoc181@gmail.com>
2024-03-23 20:18:02 -0400

    added address to the bookings migration

--- theratable-files/db/migrate/20240226025620_create_bookings.rb
+++ theratable-files/db/migrate/20240226025620_create_bookings.rb
@@ -5,14 +5,13 @@ class CreateBookings < ActiveRecord::Migration[7.0]
       t.boolean :paid #is booking paid for?
       t.integer :credits_paid #credits_paid for the booking 
       t.boolean :credits_refunded,  default: false  #credits_refunded is booking is cancelled or rescheduled and deducted after session is over
+      t.string :address #address will be filled automatically in the controller
       t.integer :account_id #account ID associated with the booking
       t.integer :schedule_id
       t.integer :session_id
       t.integer :flexologist_id
       t.integer :client_id
       t.integer :payment_id
-
-
       t.timestamps
     end
   end
--- theratable-files/db/schema.rb
+++ theratable-files/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_27_032826) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_26_022355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,21 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_032826) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "bookings", force: :cascade do |t|
-    t.string "status", default: "Not booked"
-    t.boolean "paid"
-    t.integer "credits_paid"
-    t.boolean "credits_refunded", default: false
-    t.integer "account_id"
-    t.integer "schedule_id"
-    t.integer "session_id"
-    t.integer "flexologist_id"
-    t.integer "client_id"
-    t.integer "payment_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "clients", force: :cascade do |t|
     t.string "injuries"
     t.string "goals"
@@ -52,57 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_032826) do
     t.integer "user_id"
     t.integer "session_id"
     t.integer "booking_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "flexologists", force: :cascade do |t|
-    t.string "location"
-    t.string "bio"
-    t.text "experience"
-    t.string "education"
-    t.string "profile_pic"
-    t.string "photo_gallery"
-    t.integer "user_id"
-    t.integer "schedule_id"
-    t.integer "account_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.date "date"
-    t.string "status", default: "Due"
-    t.integer "cost"
-    t.string "service", default: "60 Min Assisted Stretch Session"
-    t.integer "invoice_number"
-    t.integer "applied_credits"
-    t.integer "booking_id"
-    t.integer "account_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "schedules", force: :cascade do |t|
-    t.string "star_time"
-    t.string "end_time"
-    t.string "date"
-    t.integer "account_id"
-    t.integer "flexologist_id"
-    t.integer "booking_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sessions", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "duration"
-    t.integer "credits_worth"
-    t.boolean "credit_single", default: false
-    t.boolean "credit_bundle", default: false
-    t.integer "cost"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
